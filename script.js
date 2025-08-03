e788e039fd6385befdafef4c4a7d22ab6b36dad1
9822603752 <harshalspatil4054@gmail.com>
2025-08-03 15:46:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thanks for contacting Harshal Patil - Transportation Services! We will get back to you shortly.');
+    this.reset();
+  });
+  
